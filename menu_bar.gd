ba7fe7840df7a2682222a2547749ee9218e1b7b4
Pete Thorne <pete.thorne@learningpool.com>
2025-02-17 07:09:03 +0000

    Fix for dialog

--- menu_bar.gd
+++ menu_bar.gd
@@ -5,3 +5,7 @@ func _on_special_menu_id_pressed(id: int) -> void:
 	match id:
 		0:
 			%EmptyTrashDialog.show()
+
+
+func _on_file_menu_id_pressed(id: int) -> void:
+	pass # Replace with function body.
