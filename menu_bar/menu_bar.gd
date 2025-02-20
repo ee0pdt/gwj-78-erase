100f40e79eb1a1799768db4be386db7791723f03
Pete Thorne <pete.thorne@learningpool.com>
2025-02-20 09:57:06 +0000

    Conditionally disabled empty trash menu command

--- menu_bar/menu_bar.gd
+++ menu_bar/menu_bar.gd
@@ -1,5 +1,14 @@
 extends MenuBar
 
+@export var trash : Node
+
+
+func _process(delta: float) -> void:
+	if trash and trash._is_full():
+		%SpecialMenu.set_item_disabled(0, false)
+	else:
+		%SpecialMenu.set_item_disabled(0, true)
+
 
 func _on_special_menu_id_pressed(id: int) -> void:
 	match id:
