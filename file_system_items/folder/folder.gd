f1a7c692dbeb75661a1239214ae701a1c74bfa78
Pete Thorne <pete.thorne@learningpool.com>
2025-02-21 20:13:22 +0000

    Basic folder support

---
+++ file_system_items/folder/folder.gd
@@ -0,0 +1,22 @@
+class_name Folder
+extends FileSystemItem
+
+
+
+func _ready() -> void:
+	%Window.title = text
+
+
+func get_type() -> String:
+	return "folder"
+
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		var mouse_event = event as InputEventMouseButton
+		if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.double_click:
+			%Window.show()
+
+
+func _handle_close() -> void:
+	%Window.hide()
